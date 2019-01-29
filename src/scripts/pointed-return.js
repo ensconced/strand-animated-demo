693a11a5189b90d8367d38af6f434221f61544d4
Joe Barnett <joebarnett876@gmail.com>
2019-01-29 18:44:04 +0000

    done away with buggy PRs

--- src/scripts/pointed-return.js
+++ src/scripts/pointed-return.js
@@ -1,6 +1,4 @@
 import { reducer, collectionIntersect, format } from './knot-utils.js';
-import config from './config';
-import kldIntersections from 'kld-intersections';
 import surface from './main.js';
 
 export default function PointedReturn(options) {
@@ -47,18 +45,14 @@ PointedReturn.prototype = {
     // split at intersection point
     // concatenate part of outbound inner from before intersection,
     // with the part of inbound inner from after the intersection...
-    var outClipped = this.clippedOutboundPath(intersection, innerOutboundPolyline);
-    var inClipped = this.clippedInboundPath(intersection, innerInboundPolyline);
-    var points = outClipped.concat(inClipped).reduce(reducer, []);
+    this.outClipped = this.clippedOutboundPath(intersection, innerOutboundPolyline);
+    this.inClipped = this.clippedInboundPath(intersection, innerInboundPolyline);
+    var points = this.outClipped.concat(this.inClipped).reduce(reducer, []);
     var snp = surface.polyline(points);
     this.group.add(snp);
     format(snp);
   },
   drawOuters() {
-    function kldPoint(ext) {
-      return new kld.Point2D(ext.x, ext.y);
-    }
-
     const direction = this.pr.pr;
 
     let outerOutboundPolyline;
@@ -75,53 +69,15 @@ PointedReturn.prototype = {
       outerInboundPolyline = this.pr.point.underOutLeft || this.pr.point.overOutLeft;
     }
 
-    var d;
-    if (this.options.pr.pr === 'L') {
-      d = (config.knot.strokeWidth + config.knot.borderWidth) / 2;
-    } else {
-      d = -(config.knot.strokeWidth + config.knot.borderWidth) / 2;
-    }
+    const innerTip = this.inClipped[0];
+    const midTip = this.options.middleInbound.points[0];
+    const outerTip = {
+      x: 2 * midTip.x - innerTip.x,
+      y: 2 * midTip.y - innerTip.y,
+    };
 
-    // get pathstrs for outers
-
-    var tOutbound = 1;
-    var tInbound = 0;
-    var tStep = 0.02;
-    var outboundExtensions = [this.options.middleOutbound.offset(1, d)];
-    var inboundExtensions = [this.options.middleInbound.offset(0, d)];
-    var kld = kldIntersections;
-    var intersection = new kld.Intersection('No Intersection');
-    // build up polylines until they intersect
-    while (intersection.points.length === 0) {
-      // prepare for extension
-      tOutbound += tStep;
-      tInbound -= tStep;
-      outboundExtensions.push(this.options.middleOutbound.offset(tOutbound, d));
-      inboundExtensions.unshift(this.options.middleInbound.offset(tInbound, d));
-      var outboundPolyline = outboundExtensions.map(kldPoint);
-      var inboundPolyline = inboundExtensions.map(kldPoint);
-      // get intersection...
-      for (var n = 0; n < outboundPolyline.length - 1; n++) {
-        var a1 = outboundPolyline[n];
-        var a2 = outboundPolyline[n + 1];
-        for (var j = 0; j < inboundPolyline.length - 1; j++) {
-          var b1 = inboundPolyline[j];
-          var b2 = inboundPolyline[j + 1];
-          intersection = kld.Intersection.intersectLineLine(a1, a2, b1, b2);
-          if (intersection.points.length > 0) {
-            outboundExtensions = outboundExtensions.slice(0, n + 1);
-            inboundExtensions = inboundExtensions.slice(j + 1);
-            break;
-          }
-        }
-      }
-    }
-    // append intersection point to outbound polyline
-    var points = outerOutboundPolyline;
-    outboundExtensions.push(intersection.points[0]);
-    // concatenate inbound polyline
-    outboundExtensions = outboundExtensions.concat(inboundExtensions);
-    points = points.concat(outboundExtensions.concat(outerInboundPolyline));
+    surface.circle(outerTip.x, outerTip.y, 1).attr({ fill: 'red' });
+    const points = outerOutboundPolyline.concat(outerInboundPolyline);
     var pointList = points.reduce(reducer, []);
     var snp = surface.polyline(pointList);
     this.group.add(snp);

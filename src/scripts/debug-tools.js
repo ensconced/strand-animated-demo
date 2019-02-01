198c090762faf05e192222b126636a8de9d75a8f
Joe Barnett <joebarnett876@gmail.com>
2019-02-01 12:38:32 +0000

    debugging - bug in bezier-js reduce

--- src/scripts/debug-tools.js
+++ src/scripts/debug-tools.js
@@ -4,7 +4,7 @@ import { bezString } from './knot-utils.js';
 
 let rainbowIdx = 0;
 
-export function paint(bezier, color) {
+function paintBezier(bezier, color) {
   const ctrlPoints = bezier.points.map(coords => [coords.x, coords.y]);
   const path = bezString(...ctrlPoints);
   const svgBez = Snap(surface).path(path);
@@ -13,6 +13,31 @@ export function paint(bezier, color) {
     strokeWidth: 2,
     fill: 'none',
   });
+}
+
+function paintPoint(point, color) {
+  const circle = Snap(surface).circle(point.x, point.y, 2);
+  circle.attr({ fill: color, stroke: color });
+}
+
+export function paint(item, color) {
+  if (item.x !== undefined && item.y !== undefined) {
+    paintPoint(item, color);
+    return;
+  }
+  switch (item.constructor.name) {
+  case 'Object': // should be bezier but constructor not set in library
+    paintBezier(item, color);
+    break;
+  case 'Point2D':
+    paintPoint(item, color);
+    break;
+  case 'Array':
+    item.forEach(element => paint(element, color));
+    break;
+  default:
+    break;
+  }
 }
 
 export function rainbowPaint(bezCollection) {
